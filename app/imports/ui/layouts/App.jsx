9c87cbcf2718782ef6d0a8227d0a82f9fbf9ae6c
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-13 18:13:14 -1000

    Updated routing for Map via '/map'

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -8,6 +8,7 @@ import NavBar from '../components/global/NavBar';
 import Footer from '../components/global/Footer';
 import Landing from '../pages/Landing';
 import Submit from '../pages/SubmitPage';
+import MapPage from '../pages/Map';
 import AddStuff from '../pages/meteor-template/AddStuff';
 import EditStuff from '../pages/meteor-template/EditStuff';
 import NotFound from '../pages/NotFound';
@@ -29,6 +30,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <Route path="/giantestpage" component={giantestpage}/> {/* TODO: Remove in production */}
+              <Route path="/map" component={MapPage}/>
               <ProtectedRoute path="/submit" component={Submit}/>
               <ProtectedRoute path="/suggestions" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
