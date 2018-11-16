81599fb4d04e9133dd5bd5ea1d80ea9e6509dfd6
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-15 17:09:51 -1000

    Icon stuff

--- app/imports/ui/pages/Map.jsx
+++ app/imports/ui/pages/Map.jsx
@@ -1,27 +1,40 @@
-import 'leaflet/dist/leaflet.css';
 import React from 'react';
+import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const icon = L.icon({
+  iconUrl: 'leaf-green.png',
+  shadowUrl: 'leaf-shadow.png',
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 export default class MapPage extends React.Component {
   state = {
     lat: 51.505,
     lng: -0.09,
-    zoom: 13,
+    zoom: 18,
   }
+
+  issues = {};
 
   render() {
     const style = {
-      height: '600px',
+      height: '300px',
     };
-
-    const position = [this.state.lat, this.state.lng];
+    const centerPos = [21.2969, -157.8171];
+    // const position = [this.state.lat, this.state.lng];
     return (
-        <Map className="map" center={position} zoom={this.state.zoom} style={style}>
+        <Map className="map" center={centerPos} zoom={this.state.zoom} style={style}>
           <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
+          <Marker position={centerPos} icon={icon}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
