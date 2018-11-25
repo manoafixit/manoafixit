4b8f6ca2d287e337daac180b7b3c87ac5442fe8c
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-24 20:59:34 -1000

    Created WarningModal
    TODO: Instructions for resetting Location Permission settings for each browser

--- app/imports/ui/pages/SubmitPage.jsx
+++ app/imports/ui/pages/SubmitPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 import { Grid, Header, Segment } from 'semantic-ui-react';
 import { Bert } from 'meteor/themeteorchef:bert';
 import {
@@ -14,6 +14,7 @@ import {
   HiddenField,
 } from 'uniforms-semantic/';
 import { Issues, IssuesSchema } from '../../api/IssuesCollection/IssuesCollection';
+import WarningModal from '../components/submit/WarningModal';
 
 class SubmitPage extends React.Component {
   constructor(props) {
@@ -21,39 +22,40 @@ class SubmitPage extends React.Component {
     this.submit = this.submit.bind(this);
     this.insertCallback = this.insertCallback.bind(this);
     this.formRef = null;
+    this.state = {
+      location: {
+        lat: undefined,
+        long: undefined,
+      },
+      haveUserLocation: true,
+    };
   }
-
-  state = {
-    location: {
-      lat: undefined,
-      long: undefined,
-    },
-    haveUserLocation: false,
-  };
 
   componentDidMount() {
     // eslint-disable-next-line no-undef
-    navigator.geolocation.getCurrentPosition((position) => { // TODO: Fix this undefined ESLint error.
-      this.setState({
-        location: {
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-        },
-        haveUserLocation: true,
-      });
-    }, () => {
-      fetch('https://ipapi.co/json')
-          .then(res => res.json())
-          .then(location => {
-            this.setState({
-              location: {
-                lat: location.latitude,
-                long: location.longitude,
-              },
-              haveUserLocation: true,
-            });
+    navigator.geolocation.getCurrentPosition((position) => {
+          this.setState({
+            location: {
+              lat: position.coords.latitude,
+              long: position.coords.longitude,
+            },
+            haveUserLocation: true,
           });
-    });
+        },
+        //     () => {
+        //   fetch('https://ipapi.co/json')
+        //       .then(res => res.json())
+        //       .then(location => {
+        //         this.setState({
+        //           location: {
+        //             lat: location.latitude,
+        //             long: location.longitude,
+        //           },
+        //           haveUserLocation: true,
+        //         });
+        //       });
+        // });
+        this.setState({ haveUserLocation: false }));
   }
 
   /** Notify the user of the results of the submit. If successful, clear the form. */
@@ -79,24 +81,26 @@ class SubmitPage extends React.Component {
         <Grid container centered>
           <Grid.Column>
             <Header as="h2" textAlign="center">Submit Issue</Header>
-            <AutoForm ref={(ref) => {
-              this.formRef = ref;
-            }} schema={IssuesSchema} onSubmit={this.submit}>
-              <Segment>
-                <TextField name='title'/>
-                <LongTextField name='description'/>
-                <ListField name='tags'>
-                </ListField>
-                <SubmitField value='Submit'/>
-                <ErrorsField/>
-                <HiddenField name='likes'/>
-                <HiddenField name='status'/>
-                <HiddenField name='lat' value={this.state.location.lat}/>
-                <HiddenField name='long' value={this.state.location.long}/>
-                <HiddenField name='createdAt' value={new Date()}/>
-                <HiddenField name='owner' value='fakevalue'/>
-              </Segment>
-            </AutoForm>
+            {this.state.haveUserLocation ?
+                <AutoForm ref={(ref) => {
+                  this.formRef = ref;
+                }} schema={IssuesSchema} onSubmit={this.submit}>
+                  <Segment>
+                    <TextField name='title'/>
+                    <LongTextField name='description'/>
+                    <ListField name='tags'>
+                    </ListField>
+                    <SubmitField value='Submit'/>
+                    <ErrorsField/>
+                    <HiddenField name='likes'/>
+                    <HiddenField name='status'/>
+                    <HiddenField name='lat' value={this.state.location.lat}/>
+                    <HiddenField name='long' value={this.state.location.long}/>
+                    <HiddenField name='createdAt' value={new Date()}/>
+                    <HiddenField name='owner' value='fakevalue'/>
+                  </Segment>
+                </AutoForm>
+                : <WarningModal/>}
           </Grid.Column>
         </Grid>
     );
