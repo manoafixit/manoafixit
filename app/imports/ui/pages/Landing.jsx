ea9f5d624761e3d71c395171400a42e91e068815
gbfrancisco <grahambr@hawaii.edu>
2018-11-26 11:18:36 -1000

    Updated Landing
    Added background image
    Fixed/Removed Footer Margins

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,17 +6,17 @@ class Landing extends React.Component {
   render() {
     return (
         <div className="landing-background">
-          <Grid textAlign='center' verticalalign='middle' container stackable>
+          <Grid textAlign='center' verticalAlign='center' container stackable>
             <Grid.Row>
-              <Grid.Column width={5} veritcalalign='middle' textAlign='center'>
-                <Image size='large' src="/images/mfi-logo.png"/>
+              <Grid.Column width={5} veritcalAlign='center' textAlign='center'>
+                <Image size='large' src="/images/mfi-logo2.png"/>
               </Grid.Column>
             </Grid.Row>
 
             <Grid.Row>
               <Grid.Column width={14}>
-                <Header as='h1' size='huge'>Manoa Fix It!</Header>
-                <Header as='h2' size='small' textAlign='left'>Ever faced some staggering issues around campus? Ever
+                <Header as='h1' size='huge' inverted>Manoa Fix It!</Header>
+                <Header as='h2' size='small' textAlign='left' inverted>Ever faced some staggering issues around campus? Ever
                   wanted
                   to help solve such issues but hesistate at the last moment because it is bothersome to contact the
                   higher-ups about it? Well fear no longer! ManoaFixIt is an application that makes it easier to report
@@ -26,22 +26,22 @@ class Landing extends React.Component {
 
             <Grid.Row>
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="exclamation circle"/>
-                <Header as='h1'>Issues Feed</Header>
-                <Header as='h3'>This app shows the list of issues reported by UHM students..
+                <Icon size='huge' name="exclamation circle" inverted/>
+                <Header as='h1' inverted>Issues Feed</Header>
+                <Header as='h3' inverted>This app shows the list of issues reported by UHM students..
                 </Header>
               </Grid.Column>
 
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="feed"/>
-                <Header as='h1'>Suggestions Feed</Header>
-                <Header as='h3'>This app shows the list of issues reported by UHM students.</Header>
+                <Icon size='huge' name="feed" inverted/>
+                <Header as='h1' inverted>Suggestions Feed</Header>
+                <Header as='h3' inverted>This app shows the list of issues reported by UHM students.</Header>
               </Grid.Column>
 
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="map marker alternate"/>
-                <Header as='h1'>Heat Map</Header>
-                <Header as='h3'>It can also display a map of the campus that shows the reported issues based on urgency.
+                <Icon size='huge' name="map marker alternate" inverted/>
+                <Header as='h1' inverted>Heat Map</Header>
+                <Header as='h3' inverted>It can also display a map of the campus that shows the reported issues based on urgency.
                 </Header>
               </Grid.Column>
             </Grid.Row>
