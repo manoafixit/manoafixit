5f0682dd22265cffc483f3da0eecff117b470514
gbfrancisco <grahambr@hawaii.edu>
2018-11-26 21:35:26 -1000

    Updated FireFox Instructions

--- app/imports/ui/components/submit/WarningModalMobile.jsx
+++ app/imports/ui/components/submit/WarningModalMobile.jsx
@@ -13,14 +13,19 @@ export default class WarningModalDesktop extends Component {
 
     return (
         <div>
-          <Header>
-            Warning: You have blocked permission to access your Location
-          </Header>
-          In order to submit an issue, you must give permission to our web app to access your Location.
-          You must reset your Location Access permission settings for your browser and refresh the page
-          in order to submit an issue.
+          <div style={{ paddingTop: '10px' }}>
+            <Header>
+              Warning: You have blocked permission to access your Location
+            </Header>
+            <p>
+              In order to submit an issue, you must give permission to our web app to access your Location.
+              You must reset your Location Access permission settings for your browser and refresh the page
+              in order to submit an issue.
+            </p>
+          </div>
+
           <br/>
-          <Header as='h1'>How to reset Location Tracking permission settings (Mobile/Tablet): </Header>
+          <Header as='h2'>How to reset Location Tracking permission settings (Mobile/Tablet): </Header>
           <Segment.Group raised>
             <Segment>
               <Header as='h2'> <Icon name='chrome'/> Google Chrome </Header>
@@ -37,6 +42,7 @@ export default class WarningModalDesktop extends Component {
                 <List.Item> Click on Site Settings </List.Item>
                 <List.Item> Click on <Icon
                     name='map marker alternate'/> Location.
+                </List.Item>
                 <List.Item>
                   On the Blocked list, click on manoafixit.meteorapp.com and then click on Location Access. Then
                   click Allow to give permission for our app to track your Location.
@@ -55,11 +61,24 @@ export default class WarningModalDesktop extends Component {
               <Header as='h5'> iOS </Header>
               Instructions for iOS.
               <Header as='h5'> Android </Header>
-              Instructions for Android.
+              <List bulleted>
+                <List.Item>
+                  Click on <Icon name='ellipsis vertical'/> icon on the top right of your browser.
+                </List.Item>
+                <List.Item>
+                  Click on Settings
+                </List.Item>
+                <List.Item>
+                  Click on Clear Private Data
+                </List.Item>
+                <List.Item>
+                  Check Site settings, and click CLEAR DATA.
+                </List.Item>
+              </List>
             </Segment>
             <Segment>
-              <Header as='h4'> If you resetted your Location Tracking permission but the popup to allow
-                Location Track isnt showing, then you need to clear your browsers cookies first.</Header>
+              <Header as='h4'> If you reset your Location Tracking permission but the popup to allow
+                Location Track isn't showing, then you need to clear your browsers cookies first.</Header>
             </Segment>
           </Segment.Group>
           <Modal
@@ -69,7 +88,7 @@ export default class WarningModalDesktop extends Component {
               dimmer='blurring'
               onClose={this.close}
           >
-            <Modal.Header>Warning: You must give permission to access your Location</Modal.Header>
+            <Modal.Header> Warning: You must give permission to access your Location </Modal.Header>
             <Modal.Content>
               In order to submit an issue, you must give permission to our web app to access your Location.
               Please allow our app to access your location if there is a popup asking. Otherwise, you must reset
