c480059aed725276dbff7cdcb836b4d6ad88da3f
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-26 20:26:17 -1000

    Some progress done for the Chrome Android instructions

--- app/imports/ui/components/submit/WarningModalMobile.jsx
+++ app/imports/ui/components/submit/WarningModalMobile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, Segment, Container, Header, Icon } from 'semantic-ui-react';
+import { Button, Modal, Segment, Header, Icon, List } from 'semantic-ui-react';
 
 export default class WarningModalDesktop extends Component {
   state = {
@@ -13,47 +13,55 @@ export default class WarningModalDesktop extends Component {
 
     return (
         <div>
-          <Container>
-            <Header>
-              Warning: You have blocked permission to access your Location
-            </Header>
-            <Segment.Group>
-              <Segment>
-                In order to submit an issue, you must give permission to our web app to access your Location.
-                You must reset your Location Access permission settings for your browser and refresh the page
-                in order to submit an issue.
-                <br/>
-                <Header as='h1'>How to reset Location Tracking permission settings (Mobile/Tablet): </Header>
-              </Segment>
-              <Segment.Group raised>
-                <Segment>
-                  <Header as='h2'> <Icon name='chrome'/> Google Chrome </Header>
-                  <Header as='h5'> iOS </Header>
-                  Instructions for iOS.
-                  <Header as='h5'> Android </Header>
-                  Instructions for Android.
-                </Segment>
+          <Header>
+            Warning: You have blocked permission to access your Location
+          </Header>
+          In order to submit an issue, you must give permission to our web app to access your Location.
+          You must reset your Location Access permission settings for your browser and refresh the page
+          in order to submit an issue.
+          <br/>
+          <Header as='h1'>How to reset Location Tracking permission settings (Mobile/Tablet): </Header>
+          <Segment.Group raised>
+            <Segment>
+              <Header as='h2'> <Icon name='chrome'/> Google Chrome </Header>
+              <Header as='h5'> iOS </Header>
+              Instructions for iOS.
+              <Header as='h5'> Android </Header>
+              <List bulleted>
+                <List.Item>
+                  Click on <Icon name='ellipsis vertical'/> icon on the top right of your browser.
+                </List.Item>
+                <List.Item>
+                  Click on Settings
+                </List.Item>
+                <List.Item> Click on Site Settings </List.Item>
+                <List.Item> Click on <Icon
+                    name='map marker alternate'/> Location.
+                <List.Item>
+                  On the Blocked list, click on manoafixit.meteorapp.com and then click on Location Access. Then
+                  click Allow to give permission for our app to track your Location.
+                </List.Item>
+              </List>
+            </Segment>
 
-                <Segment>
-                  <Header as='h2'> <Icon name='safari'/> Safari </Header>
-                  <Header as='h5'> iOS </Header>
-                  Instructions for iOS.
-                </Segment>
+            <Segment>
+              <Header as='h2'> <Icon name='safari'/> Safari </Header>
+              <Header as='h5'> iOS </Header>
+              Instructions for iOS.
+            </Segment>
 
-                <Segment>
-                  <Header as='h2'> <Icon name='firefox'/> Mozilla Firefox </Header>
-                  <Header as='h5'> iOS </Header>
-                  Instructions for iOS.
-                  <Header as='h5'> Android </Header>
-                  Instructions for Android.
-                </Segment>
-                <Segment>
-                  <Header as='h4'> If you resetted your Location Tracking permission but the popup to allow
-                    Location Track isnt showing, then you need to clear your browsers cookies first.</Header>
-                </Segment>
-              </Segment.Group>
-            </Segment.Group>
-          </Container>
+            <Segment>
+              <Header as='h2'> <Icon name='firefox'/> Mozilla Firefox </Header>
+              <Header as='h5'> iOS </Header>
+              Instructions for iOS.
+              <Header as='h5'> Android </Header>
+              Instructions for Android.
+            </Segment>
+            <Segment>
+              <Header as='h4'> If you resetted your Location Tracking permission but the popup to allow
+                Location Track isnt showing, then you need to clear your browsers cookies first.</Header>
+            </Segment>
+          </Segment.Group>
           <Modal
               open={open}
               closeOnEscape={false}
