4429dababa36f5102f777fd35f60e10e65f6228c
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-28 22:03:58 -1000

    Implemented Filter by Issue Status

--- app/imports/ui/pages/feed/FeedPageDesktop.jsx
+++ app/imports/ui/pages/feed/FeedPageDesktop.jsx
@@ -9,12 +9,6 @@ import FeedRow from '../../components/feed/FeedRow';
 import SubmitButton from '../../components/feed/SubmitButton';
 
 const fakeData = [];
-const sortOptions = [
-  { key: 1, text: 'Newest', value: 1 },
-  { key: 2, text: 'Oldest', value: 2 },
-  { key: 3, text: 'Most Liked', value: 3 },
-  { key: 4, text: 'Least Liked', value: 4 },
-];
 
 class FeedPageDesktop extends React.Component {
   constructor(props) {
@@ -24,6 +18,7 @@ class FeedPageDesktop extends React.Component {
         isLoading: false,
       },
       sort: 1,
+      filter: 0,
     };
   }
 
@@ -52,20 +47,25 @@ class FeedPageDesktop extends React.Component {
 
   handleSortChange = (e, { value }) => this.setState({ sort: value });
 
-  switch() {
+  sortBy() {
     let result = [];
+    let filtered;
     switch (this.state.sort) {
       case 1:
-        result = this.props.issuesNewest.map((issue, index) => <FeedRow key={index} issue={issue}/>);
+        filtered = this.filterBy(this.props.issuesNewest);
+        result = filtered.map((issue, index) => <FeedRow key={index} issue={issue}/>);
         break;
       case 2:
-        result = this.props.issuesOldest.map((issue, index) => <FeedRow key={index} issue={issue}/>);
+        filtered = this.filterBy(this.props.issuesOldest);
+        result = filtered.map((issue, index) => <FeedRow key={index} issue={issue}/>);
         break;
       case 3:
-        result = this.props.issuesMostLiked.map((issue, index) => <FeedRow key={index} issue={issue}/>);
+        filtered = this.filterBy(this.props.issuesMostLiked);
+        result = filtered.map((issue, index) => <FeedRow key={index} issue={issue}/>);
         break;
       case 4:
-        result = this.props.issuesLeastLiked.map((issue, index) => <FeedRow key={index} issue={issue}/>);
+        filtered = this.filterBy(this.props.issuesLeastLiked);
+        result = filtered.map((issue, index) => <FeedRow key={index} issue={issue}/>);
         break;
       default:
         throw new Meteor.Error('Error in switch() somehow');
@@ -73,21 +73,58 @@ class FeedPageDesktop extends React.Component {
     return result;
   }
 
+  handleFilterChange = (e, { value }) => this.setState({ filter: value });
+
+  filterBy(issues) {
+    switch (this.state.filter) {
+      case 0:
+        return issues;
+      case 1:
+        return _.filter(issues, (issue) => issue.status === 'Open');
+      case 2:
+        return _.filter(issues, (issue) => issue.status === 'Acknowledged');
+      case 3:
+        return _.filter(issues, (issue) => issue.status === 'Ongoing');
+      case 4:
+        return _.filter(issues, (issue) => issue.status === 'Resolved');
+      case 5:
+        return _.filter(issues, (issue) => issue.status === 'Removed');
+      default:
+        throw new Meteor.Error('Invalid status');
+    }
+  }
+
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting Issue Data</Loader>;
   }
 
   renderPage() {
-    const { isLoading } = this.state;
     const wrapperStyle = {
       paddingTop: '20px',
       paddingBottom: '50px',
     };
-
     const menuHeaderStyle = {
       boxShadow: 'none',
       border: 'none',
     };
+
+    const { isLoading } = this.state;
+
+    const sortOptions = [
+      { key: 1, text: 'Newest', value: 1 },
+      { key: 2, text: 'Oldest', value: 2 },
+      { key: 3, text: 'Most Liked', value: 3 },
+      { key: 4, text: 'Least Liked', value: 4 },
+    ];
+
+    const filterOptions = [
+      { key: 0, text: 'All Issues', value: 0 },
+      { key: 1, text: 'Open Issues', value: 1 },
+      { key: 2, text: 'Acknowledged Issues', value: 2 },
+      { key: 3, text: 'Ongoing Issues', value: 3 },
+      { key: 4, text: 'Resolved Issues', value: 4 },
+      { key: 5, text: 'Removed Issues', value: 5 },
+    ];
 
     return (
         <div className='wrapper' style={wrapperStyle}>
@@ -103,6 +140,13 @@ class FeedPageDesktop extends React.Component {
                 <Search
                     loading={isLoading}
                     onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+                />
+              </Menu.Item>
+              <Menu.Item>
+                <Dropdown
+                    placeholder='Filter Issues'
+                    options={filterOptions}
+                    onChange={this.handleFilterChange}
                 />
               </Menu.Item>
               <Menu.Item position='right'>
@@ -129,7 +173,7 @@ class FeedPageDesktop extends React.Component {
                 </Table.Row>
               </Table.Header>
               <Table.Body>
-                {this.switch()}
+                {this.sortBy()}
               </Table.Body>
             </Table>
           </Container>
