66712790a7b84cdd09f50bf04a0220203ad1b230
Gian Paolo Calica <gcalica@hawaii.edu>
2018-11-28 20:02:18 -1000

    Fix ESLint errors

--- app/imports/ui/components/landing/SegmentTwo.jsx
+++ app/imports/ui/components/landing/SegmentTwo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Header, Icon } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class SegmentTwo extends React.Component {
@@ -11,11 +11,10 @@ class SegmentTwo extends React.Component {
             <Grid.Row>
               <Grid.Column width={14}>
                 <Header as='h1' style={{ fontSize: '40px' }} inverted>Manoa Fix It!</Header>
-                <Header as='h2' style={{ fontSize: '20px' }} textAlign='center' inverted>Ever faced some staggering issues around campus? Ever
-                  wanted
-                  to help solve such issues but hesistate at the last moment because it is bothersome to contact the
-                  higher-ups about it? Well fear no longer! ManoaFixIt is an application that makes it easier to report
-                  problems around campus. It includes...</Header>
+                <Header as='h2' style={{ fontSize: '20px' }} textAlign='center' inverted>Ever faced some staggering
+                  issues around campus? Ever wanted to help solve such issues but hesistate at the last moment
+                  because it is bothersome to contact the higher-ups about it? Well fear no longer! ManoaFixIt
+                  is an application that makes it easier to report problems around campus. It includes...</Header>
               </Grid.Column>
             </Grid.Row>
 
