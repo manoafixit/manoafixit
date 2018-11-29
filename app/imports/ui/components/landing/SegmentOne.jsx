6a2a82b0966afdb56edde31e5fc4ab1f80348553
gbfrancisco <grahambr@hawaii.edu>
2018-11-29 00:01:40 -1000

    Implemented Buttons on Landing
    Added Sign in, Sign out, when not logged in. Added Submit when logged in.

--- app/imports/ui/components/landing/SegmentOne.jsx
+++ app/imports/ui/components/landing/SegmentOne.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { NavLink, withRouter } from 'react-router-dom';
+import { Grid, Image, Button } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown';
 
 /** A simple static component to render some text for the landing page. */
 class SegmentOne extends React.Component {
@@ -11,9 +16,32 @@ class SegmentOne extends React.Component {
             <Grid.Row>
               <Grid.Column width={5} veritcalAlign='center' textAlign='center'>
                 <div style={{ paddingTop: '100px' }}>
-                <Image size='large' src="/images/mfi-logo2.png"/>
+                  <Image size='large' src="/images/mfi-logo2.png"/>
                 </div>
               </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              {this.props.currentUser === '' ? (
+                  <div>
+                    <Grid.Column>
+                      <Button as={NavLink} exact to="/signin">
+                        Sign In
+                      </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Button as={NavLink} exact to="/signup">
+                        Sign Up
+                      </Button>
+                    </Grid.Column>
+                  </div>
+              ) : (
+                  <Grid.Column>
+                    <Button as={NavLink} exact to="/submit">
+                      Submit an Issue
+                    </Button>
+                  </Grid.Column>
+              )}
             </Grid.Row>
 
           </Grid>
@@ -22,4 +50,12 @@ class SegmentOne extends React.Component {
   }
 }
 
-export default SegmentOne;
+SegmentOne.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+const SegmentOneContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(SegmentOne);
+
+export default withRouter(SegmentOneContainer);
