8cf14b2ce1a0575a30dc4fc4b06ef109ee622d4f
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-02 16:57:16 -1000

    Added SuperAdmin to Admin Role as a temporary fix

--- app/imports/api/UsersCollection/UsersCollection.js
+++ app/imports/api/UsersCollection/UsersCollection.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
 import { ROLE } from '../Roles/Roles';
 import BaseCollection from '../BaseCollection/BaseCollection';
