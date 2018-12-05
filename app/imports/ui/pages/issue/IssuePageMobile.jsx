9b21aab3525cce40ff571f2c55dca8c0bc7fe919
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-05 07:54:28 -1000

    Restyled Issue Page Mobile

--- app/imports/ui/pages/issue/IssuePageMobile.jsx
+++ app/imports/ui/pages/issue/IssuePageMobile.jsx
@@ -1,87 +1,78 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Container, Grid, Header, Menu } from 'semantic-ui-react';
+import { Grid, Header, Menu, Responsive } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { ROLE } from '../../../api/Roles/Roles';
+import OriginalPostMobile from '../../components/issue/responsive/OriginalPostMobile';
 import ReplyBox from '../../components/issue/ReplyBox';
 import Replies from '../../components/issue/Replies';
-import OriginalPostMobile from '../../components/issue/responsive/OriginalPostMobile';
 import Status from '../../components/global/issue/Status';
 import Likes from '../../components/global/issue/Likes';
+import AdminStatusChange from '../../components/global/issue/AdminStatusChange';
 
 class IssuePageMobile extends React.Component {
   render() {
     const wrapperStyle = {
       paddingTop: '30px',
       paddingBottom: '30px',
-      // wordWrap: 'break-word',
-    };
-    const issueWrapperStyle = {
       wordWrap: 'break-word',
+    };
+
+    const wordWrapperStyle = {
+      wordWrap: 'break-word',
+      maxWidth: '320px',
     };
 
     const menuStyle = {
       border: 'none',
       boxShadow: 'none',
     };
+
     const date = format(this.props.issue.createdAt, 'MMMM D, YYYY, h:mm aa');
 
-    { /* <div style={wrapperStyle}> */
-    }
-    { /* <Container> */
-    }
-    { /* <Grid> */
-    }
-    { /* <Grid.Row> */
-    }
-    { /* <div style={issueWrapperStyle}> */
-    }
-    { /* <OriginalPostMobile issue={this.props.issue}/> */
-    }
-    { /* </div> */
-    }
-    { /* </Grid.Row> */
-    }
-
-    { /* <Grid.Row> */
-    }
-    { /* <ReplyBox issue={this.props.issue}/> */
-    }
-    { /* </Grid.Row> */
-    }
-
-    { /* <Grid.Row> */
-    }
-    { /* <Replies issue={this.props.issue} replies={this.props.replies}/> */
-    }
-    { /* </Grid.Row> */
-    }
-    { /* </Grid> */
-    }
-    { /* </Container> */
-    }
-    { /* </div> */
-    }
     return (
         <div style={wrapperStyle}>
-              <Grid.Row>
-                <Menu borderless style={menuStyle}>
-                  <Menu.Item>
-                    <Status issue={this.props.issue}/>
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Likes issue={this.props.issue}/>
-                  </Menu.Item>
-                </Menu>
-              </Grid.Row>
-              <Grid.Row>
-                <Header style={issueWrapperStyle}>
-                  {this.props.issue.title}
-                  <Header.Subheader>
-                    {this.props.issue.owner} opened this issue on {date}
-                  </Header.Subheader>
-                </Header>
-              </Grid.Row>
+          <Grid.Row>
+            <Menu borderless style={menuStyle}>
+              <Menu.Item>
+                {(Roles.userIsInRole(Meteor.userId(), ROLE.SUPERADMIN)
+                    || Roles.userIsInRole(Meteor.userId(), ROLE.ADMIN)) ?
+                    <AdminStatusChange issue={this.props.issue}/>
+                    : <Status issue={this.props.issue}/>}
+              </Menu.Item>
+              <Menu.Item position='right'>
+                <Likes issue={this.props.issue}/>
+              </Menu.Item>
+            </Menu>
+          </Grid.Row>
+
+          <Grid.Row>
+            <div style={wordWrapperStyle}>
+              <Header attached='top'>
+                {this.props.issue.title}
+                <Header.Subheader>
+                  {this.props.issue.owner} opened this issue on {date}
+                </Header.Subheader>
+              </Header>
+            </div>
+          </Grid.Row>
+
+          <Grid.Row>
+            <div style={wordWrapperStyle}>
+              <OriginalPostMobile issue={this.props.issue}/>
+            </div>
+          </Grid.Row>
+
+          <Grid.Row>
+            <ReplyBox issue={this.props.issue}/>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Replies issue={this.props.issue} replies={this.props.replies}/>
+          </Grid.Row>
         </div>
     );
   }
