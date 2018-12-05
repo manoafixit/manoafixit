5df0b6d63349f58244682d08ea98780714c3b7dc
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-05 07:18:28 -1000

    Minor progress

--- app/imports/ui/pages/issue/IssuePageMobile.jsx
+++ app/imports/ui/pages/issue/IssuePageMobile.jsx
@@ -1,39 +1,87 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Header, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import OriginalPost from '../../components/issue/OriginalPost';
+import { format } from 'date-fns';
 import ReplyBox from '../../components/issue/ReplyBox';
 import Replies from '../../components/issue/Replies';
+import OriginalPostMobile from '../../components/issue/responsive/OriginalPostMobile';
+import Status from '../../components/global/issue/Status';
+import Likes from '../../components/global/issue/Likes';
 
 class IssuePageMobile extends React.Component {
   render() {
     const wrapperStyle = {
       paddingTop: '30px',
       paddingBottom: '30px',
-      wordWrap: 'break-word',
+      // wordWrap: 'break-word',
     };
     const issueWrapperStyle = {
       wordWrap: 'break-word',
     };
 
+    const menuStyle = {
+      border: 'none',
+      boxShadow: 'none',
+    };
+    const date = format(this.props.issue.createdAt, 'MMMM D, YYYY, h:mm aa');
+
+    { /* <div style={wrapperStyle}> */
+    }
+    { /* <Container> */
+    }
+    { /* <Grid> */
+    }
+    { /* <Grid.Row> */
+    }
+    { /* <div style={issueWrapperStyle}> */
+    }
+    { /* <OriginalPostMobile issue={this.props.issue}/> */
+    }
+    { /* </div> */
+    }
+    { /* </Grid.Row> */
+    }
+
+    { /* <Grid.Row> */
+    }
+    { /* <ReplyBox issue={this.props.issue}/> */
+    }
+    { /* </Grid.Row> */
+    }
+
+    { /* <Grid.Row> */
+    }
+    { /* <Replies issue={this.props.issue} replies={this.props.replies}/> */
+    }
+    { /* </Grid.Row> */
+    }
+    { /* </Grid> */
+    }
+    { /* </Container> */
+    }
+    { /* </div> */
+    }
     return (
-        <div style= {wrapperStyle}>
-          <Container>
-            <Grid>
+        <div style={wrapperStyle}>
               <Grid.Row>
-            <div style={issueWrapperStyle}>
-              <OriginalPost issue={this.props.issue}/>
-            </div>
+                <Menu borderless style={menuStyle}>
+                  <Menu.Item>
+                    <Status issue={this.props.issue}/>
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Likes issue={this.props.issue}/>
+                  </Menu.Item>
+                </Menu>
               </Grid.Row>
               <Grid.Row>
-            <ReplyBox issue={this.props.issue}/>
+                <Header style={issueWrapperStyle}>
+                  {this.props.issue.title}
+                  <Header.Subheader>
+                    {this.props.issue.owner} opened this issue on {date}
+                  </Header.Subheader>
+                </Header>
               </Grid.Row>
-              <Grid.Row>
-            <Replies issue={this.props.issue} replies={this.props.replies}/>
-              </Grid.Row>
-            </Grid>
-          </Container>
         </div>
     );
   }
