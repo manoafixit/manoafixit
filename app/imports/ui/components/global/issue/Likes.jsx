c85086b8ba22bffa72b5c31b54719d5c04609826
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-05 06:26:07 -1000

    Implemented liking issue feature #67

--- app/imports/ui/components/global/issue/Likes.jsx
+++ app/imports/ui/components/global/issue/Likes.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/erasaur:meteor-lodash';
 import { Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Issues } from '../../../../api/IssuesCollection/IssuesCollection';
 
 class Likes extends React.Component {
   // Check if the user has already liked this issue
   hasLiked() {
     const userID = Meteor.userId();
-    if (this.props.issue.likedBy !== undefined) {
-      return this.props.issue.likedBy[userID];
+    if (_.indexOf(this.props.issue.likedBy, userID) >= 0) {
+      return true;
     }
     return false;
   }
 
-  handleLikeIssue = () => {
+  likedCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Failed to Like the Issue: ${error.message}. Please screenshot 
+      this error and send to gcalica@hawaii.edu` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Liked the Issue' });
+    }
+  }
 
+  unlikedCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Failed to Unlike the Issue: ${error.message} Please screenshot 
+      this error and send to gcalica@hawaii.edu` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Unliked the Issue' });
+    }
+  }
+
+  handleLikeIssue = () => {
+    const userID = Meteor.userId();
+    Issues.increaseLikes(this.props.issue._id, userID, undefined, this.likedCallback);
   };
 
   handleUnlikeIssue = () => {
-
+    const userID = Meteor.userId();
+    Issues.decreaseLikes(this.props.issue._id, userID, undefined, this.unlikedCallback);
   };
 
   render() {
