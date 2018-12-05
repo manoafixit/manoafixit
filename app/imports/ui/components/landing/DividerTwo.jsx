dabd91d3157cf75d81edf33b070fb0f1ba89cf99
gbfrancisco <grahambr@hawaii.edu>
2018-12-04 22:39:43 -1000

    DividerTwo Added

--- app/imports/ui/components/landing/DividerTwo.jsx
+++ app/imports/ui/components/landing/DividerTwo.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class DividerTwo extends React.Component {
   render() {
     return (
-        <div className="landing-background">
+        <div className="divider-background">
           <Grid textAlign='center' verticalAlign='center' container stackable>
 
             <Grid.Row>
-              <Grid.Column width={5} veritcalAlign='center' textAlign='center'>
-                <div style={{ paddingTop: '100px' }}>
-                  <Image size='large' src="/images/mfi-logo2.png"/>
-                </div>
+              <Grid.Column width={5} verticalAlign='top' textAlign='center'>
+                <Icon size='huge' name="exclamation circle" inverted/>
+                <Header as='h1' inverted>Issues Feed</Header>
+                <Header as='h3' inverted>This app shows the list of issues reported by UHM students..
+                </Header>
+              </Grid.Column>
+
+              <Grid.Column width={5} verticalAlign='top' textAlign='center'>
+                <Icon size='huge' name="feed" inverted/>
+                <Header as='h1' inverted>Suggestions Feed</Header>
+                <Header as='h3' inverted>This app shows the list of issues reported by UHM students.</Header>
+              </Grid.Column>
+
+              <Grid.Column width={5} verticalAlign='top' textAlign='center'>
+                <Icon size='huge' name="map marker alternate" inverted/>
+                <Header as='h1' inverted>Heat Map</Header>
+                <Header as='h3' inverted>It can also display a map of the campus that shows the reported issues
+                  based on urgency.
+                </Header>
               </Grid.Column>
             </Grid.Row>
 
