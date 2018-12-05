bb6c1ce96d3cce25d4e49ff6fc0d92176e1c480f
Adrian Au <auadrian@hawaii.edu>
2018-12-05 01:13:20 -1000

    Fixed ESLint error

--- app/imports/ui/components/issue/OriginalPost.jsx
+++ app/imports/ui/components/issue/OriginalPost.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Segment, Menu, Icon, Message } from 'semantic-ui-react';
+import { Header, Segment, Menu, Message } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { format } from 'date-fns';
