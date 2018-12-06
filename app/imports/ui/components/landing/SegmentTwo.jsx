37ee16eeabc2be4b3e270d035cf6498a08f9d0af
gbfrancisco <grahambr@hawaii.edu>
2018-12-05 19:12:09 -1000

    Added Images and Updated Descriptions

--- app/imports/ui/components/landing/SegmentTwo.jsx
+++ app/imports/ui/components/landing/SegmentTwo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Grid, Header, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class SegmentTwo extends React.Component {
@@ -10,23 +10,26 @@ class SegmentTwo extends React.Component {
 
             <Grid.Row>
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="feed" inverted/>
+                <Image size='medium' src="/images/submit.png" centered rounded/>
                 <Header as='h1' inverted>Submit Issues</Header>
-                <Header as='h3' inverted>This app shows the list of issues reported by UHM students.</Header>
+                <Header as='h3' inverted>With this app, you can easily submit any issues you find around campus. You can
+                  easily do it on desktop or on mobile!</Header>
               </Grid.Column>
 
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="exclamation circle" inverted/>
+                <Image size='medium' src="/images/feed.png" centered rounded/>
                 <Header as='h1' inverted>Issues Feed</Header>
-                <Header as='h3' inverted>This app shows the list of issues reported by UHM students.
+                <Header as='h3' inverted>Your submission will appear on a feed page which everybody can see. The more
+                  awareness towards the issue, the quicker it will be fixed.
                 </Header>
               </Grid.Column>
 
               <Grid.Column width={5} verticalAlign='top' textAlign='center'>
-                <Icon size='huge' name="map marker alternate" inverted/>
-                <Header as='h1' inverted>Issues Map</Header>
-                <Header as='h3' inverted>It can also display a map of the campus that shows the reported issues
-                  indicated by markers.
+                <Image size='medium' src="/images/issue.png" centered rounded/>
+                <Header as='h1' inverted>Issue Page</Header>
+                <Header as='h3' inverted>Every issue has its own dedicated page. By clicking on a certain issue on the
+                  feed, you will be redirected to a page containing the issue's location and description. People can
+                  also comment their views towards the issue in this page.
                 </Header>
               </Grid.Column>
             </Grid.Row>
