cb9770602fe3b1b8b96a6fc5f70cf4ff147e77ae
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-06 01:59:11 -1000

    Fix rendering of replies #108

--- app/imports/ui/components/issue/Replies.jsx
+++ app/imports/ui/components/issue/Replies.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { _ } from 'meteor/erasaur:meteor-lodash';
 import Reply from './Reply';
 import AdminStatusReply from './AdminStatusReply';
 
 /** Renders a table containing all of the Contacts documents. Use <Contact> to render each row. */
 class Replies extends React.Component {
   render() {
-    const normal_replies = _.filter(this.props.replies, (reply) => reply.admin_status === false);
-    const admin_replies = _.filter(this.props.replies, (reply) => reply.admin_status === true);
     return (
         <div>
-          {normal_replies.map((reply, index) => <Reply key={index} reply={reply}/>)}
-          {admin_replies.map((reply, index) => <AdminStatusReply key={index} reply={reply}/>)}
+          {this.props.replies.map((reply, index) => {
+            if (reply.admin_status === false) {
+              return <Reply key={index} reply={reply}/>;
+            }
+            return <AdminStatusReply key={index} reply={reply}/>;
+          })}
         </div>
     );
   }
