d543df1bd5045a0164d79ca2be1021c3f245c008
gbfrancisco <grahambr@hawaii.edu>
2018-12-05 18:28:38 -1000

    Separated desktop and mobile footer

--- app/imports/ui/components/global/Footer.jsx
+++ app/imports/ui/components/global/Footer.jsx
@@ -1,49 +1,20 @@
 import React from 'react';
-import { Grid, Icon } from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
+import FooterMobile from './footer/FooterMobile';
+import FooterDesktop from './footer/FooterDesktop';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px', color: 'white' };
 
     return (
-        <div className="footer-background">
-          <footer>
-            <div style={divStyle} className="ui center aligned container">
-              <Grid columns={8} centered divided stackable>
-                <Grid.Row centered verticalAlign='middle'>
-                  CHECK OUR GITHUB PAGES
-                  <Icon style={{paddingLeft: '10px'}} name='github' size='large'/>
-                </Grid.Row>
-
-                <Grid.Row centered textAlign='center'>
-                  <Grid.Column textAlign='center'>
-                    <a href='https://github.com/manoafixit/manoafixit'>
-                      ManoaFixIt
-                    </a>
-                  </Grid.Column>
-
-                  <Grid.Column textAlign='center'>
-                    <a href='https://github.com/Au-Adrian'>
-                      Adrian Au
-                    </a>
-                  </Grid.Column>
-
-                  <Grid.Column textAlign='center'>
-                    <a href='https://github.com/gcalica'>
-                      Gian Calica
-                    </a>
-                  </Grid.Column>
-
-                  <Grid.Column textAlign='center'>
-                    <a href='https://github.com/gbfrancisco'>
-                      Graham Francisco
-                    </a>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </div>
-          </footer>
+        <div>
+          <Responsive {...Responsive.onlyMobile}>
+            <FooterMobile/>
+          </Responsive>
+          <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            <FooterDesktop/>
+          </Responsive>
         </div>
     );
   }
