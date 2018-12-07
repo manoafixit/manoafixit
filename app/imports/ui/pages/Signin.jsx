e12362d99be213d9455eb9d9382e7c025c681432
gbfrancisco <grahambr@hawaii.edu>
2018-12-06 18:04:45 -1000

    Revert "Update text"
    This reverts commit eb39edd45d5fa3b1eec353490a468e2f87e6eb47.

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -97,7 +97,7 @@ export default class Signin extends React.Component {
             </Grid.Column>
             <Grid.Row>
             <Header as="h3" textAlign="center" style={headerStyle}>
-              Help Fix Manoa.
+              Why Manoafixit?
             </Header>
             </Grid.Row>
           </Grid>
