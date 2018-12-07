f07e349ec5d611ae697d4fe041de5784dcdb0c4d
Gian Paolo Calica <gcalica@hawaii.edu>
2018-12-06 18:49:28 -1000

    Fix ESLint errors

--- app/imports/ui/pages/dashboard/Dashboard.jsx
+++ app/imports/ui/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ResponsiveBar } from '@nivo/bar'
-import { ResponsiveCalendar } from '@nivo/calendar'
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveBar } from '@nivo/bar';
+import { ResponsiveCalendar } from '@nivo/calendar';
+import { ResponsiveLine } from '@nivo/line';
 import { Header, Segment } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { fakeData, fakeData2, fakeData3 } from './TestData';
@@ -23,73 +23,73 @@ class Dashboard extends React.Component {
             <ResponsiveBar
                 data={fakeData}
                 keys={[
-                  "hot dog",
-                  "burger",
-                  "sandwich",
-                  "kebab",
-                  "fries",
-                  "donut"
+                  'hot dog',
+                  'burger',
+                  'sandwich',
+                  'kebab',
+                  'fries',
+                  'donut',
                 ]}
                 indexBy="country"
                 margin={{
-                  "top": 50,
-                  "right": 130,
-                  "bottom": 50,
-                  "left": 60
+                  top: 50,
+                  right: 130,
+                  bottom: 50,
+                  left: 60,
                 }}
                 padding={0.3}
                 colors="nivo"
                 colorBy="id"
                 defs={[
                   {
-                    "id": "dots",
-                    "type": "patternDots",
-                    "background": "inherit",
-                    "color": "#38bcb2",
-                    "size": 4,
-                    "padding": 1,
-                    "stagger": true
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true,
                   },
                   {
-                    "id": "lines",
-                    "type": "patternLines",
-                    "background": "inherit",
-                    "color": "#eed312",
-                    "rotation": -45,
-                    "lineWidth": 6,
-                    "spacing": 10
-                  }
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10,
+                  },
                 ]}
                 fill={[
                   {
-                    "match": {
-                      "id": "fries"
+                    match: {
+                      id: 'fries',
                     },
-                    "id": "dots"
+                    id: 'dots',
                   },
                   {
-                    "match": {
-                      "id": "sandwich"
+                    match: {
+                      id: 'sandwich',
                     },
-                    "id": "lines"
-                  }
+                    id: 'lines',
+                  },
                 ]}
                 borderColor="inherit:darker(1.6)"
                 axisBottom={{
-                  "tickSize": 5,
-                  "tickPadding": 5,
-                  "tickRotation": 0,
-                  "legend": "country",
-                  "legendPosition": "middle",
-                  "legendOffset": 32
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'country',
+                  legendPosition: 'middle',
+                  legendOffset: 32,
                 }}
                 axisLeft={{
-                  "tickSize": 5,
-                  "tickPadding": 5,
-                  "tickRotation": 0,
-                  "legend": "food",
-                  "legendPosition": "middle",
-                  "legendOffset": -40
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'food',
+                  legendPosition: 'middle',
+                  legendOffset: -40,
                 }}
                 labelSkipWidth={12}
                 labelSkipHeight={12}
@@ -99,27 +99,27 @@ class Dashboard extends React.Component {
                 motionDamping={15}
                 legends={[
                   {
-                    "dataFrom": "keys",
-                    "anchor": "bottom-right",
-                    "direction": "column",
-                    "justify": false,
-                    "translateX": 120,
-                    "translateY": 0,
-                    "itemsSpacing": 2,
-                    "itemWidth": 100,
-                    "itemHeight": 20,
-                    "itemDirection": "left-to-right",
-                    "itemOpacity": 0.85,
-                    "symbolSize": 20,
-                    "effects": [
+                    dataFrom: 'keys',
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 120,
+                    translateY: 0,
+                    itemsSpacing: 2,
+                    itemWidth: 100,
+                    itemHeight: 20,
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 0.85,
+                    symbolSize: 20,
+                    effects: [
                       {
-                        "on": "hover",
-                        "style": {
-                          "itemOpacity": 1
-                        }
-                      }
-                    ]
-                  }
+                        on: 'hover',
+                        style: {
+                          itemOpacity: 1,
+                        },
+                      },
+                    ],
+                  },
                 ]}
             />
           </Segment>
@@ -127,37 +127,37 @@ class Dashboard extends React.Component {
             <ResponsiveLine
                 data={fakeData2}
                 margin={{
-                  "top": 50,
-                  "right": 110,
-                  "bottom": 50,
-                  "left": 60
+                  top: 50,
+                  right: 110,
+                  bottom: 50,
+                  left: 60,
                 }}
                 xScale={{
-                  "type": "point"
+                  type: 'point',
                 }}
                 yScale={{
-                  "type": "linear",
-                  "stacked": true,
-                  "min": "auto",
-                  "max": "auto"
+                  type: 'linear',
+                  stacked: true,
+                  min: 'auto',
+                  max: 'auto',
                 }}
                 axisBottom={{
-                  "orient": "bottom",
-                  "tickSize": 5,
-                  "tickPadding": 5,
-                  "tickRotation": 0,
-                  "legend": "transportation",
-                  "legendOffset": 36,
-                  "legendPosition": "middle"
+                  orient: 'bottom',
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'transportation',
+                  legendOffset: 36,
+                  legendPosition: 'middle',
                 }}
                 axisLeft={{
-                  "orient": "left",
-                  "tickSize": 5,
-                  "tickPadding": 5,
-                  "tickRotation": 0,
-                  "legend": "count",
-                  "legendOffset": -40,
-                  "legendPosition": "middle"
+                  orient: 'left',
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                  legend: 'count',
+                  legendOffset: -40,
+                  legendPosition: 'middle',
                 }}
                 dotSize={10}
                 dotColor="inherit:darker(0.3)"
@@ -171,29 +171,29 @@ class Dashboard extends React.Component {
                 motionDamping={15}
                 legends={[
                   {
-                    "anchor": "bottom-right",
-                    "direction": "column",
-                    "justify": false,
-                    "translateX": 100,
-                    "translateY": 0,
-                    "itemsSpacing": 0,
-                    "itemDirection": "left-to-right",
-                    "itemWidth": 80,
-                    "itemHeight": 20,
-                    "itemOpacity": 0.75,
-                    "symbolSize": 12,
-                    "symbolShape": "circle",
-                    "symbolBorderColor": "rgba(0, 0, 0, .5)",
-                    "effects": [
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
                       {
-                        "on": "hover",
-                        "style": {
-                          "itemBackground": "rgba(0, 0, 0, .03)",
-                          "itemOpacity": 1
-                        }
-                      }
-                    ]
-                  }
+                        on: 'hover',
+                        style: {
+                          itemBackground: 'rgba(0, 0, 0, .03)',
+                          itemOpacity: 1,
+                        },
+                      },
+                    ],
+                  },
                 ]}
             />
           </Segment>
@@ -204,16 +204,16 @@ class Dashboard extends React.Component {
                 to="2016-07-12"
                 emptyColor="#eeeeee"
                 colors={[
-                  "#61cdbb",
-                  "#97e3d5",
-                  "#e8c1a0",
-                  "#f47560"
+                  '#61cdbb',
+                  '#97e3d5',
+                  '#e8c1a0',
+                  '#f47560',
                 ]}
                 margin={{
-                  "top": 100,
-                  "right": 30,
-                  "bottom": 60,
-                  "left": 30
+                  top: 100,
+                  right: 30,
+                  bottom: 60,
+                  left: 30,
                 }}
                 yearSpacing={40}
                 monthBorderColor="#ffffff"
@@ -222,14 +222,14 @@ class Dashboard extends React.Component {
                 dayBorderColor="#ffffff"
                 legends={[
                   {
-                    "anchor": "bottom-right",
-                    "direction": "row",
-                    "translateY": 36,
-                    "itemCount": 4,
-                    "itemWidth": 34,
-                    "itemHeight": 36,
-                    "itemDirection": "top-to-bottom"
-                  }
+                    anchor: 'bottom-right',
+                    direction: 'row',
+                    translateY: 36,
+                    itemCount: 4,
+                    itemWidth: 34,
+                    itemHeight: 36,
+                    itemDirection: 'top-to-bottom',
+                  },
                 ]}
             />
           </Segment>
